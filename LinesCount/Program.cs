2046228663f77bbac3c138c8cf880b4318d02c4c
Михаил Товарнов <misha198921rip@gmail.com>
2017-08-16 18:37:55 +0300

    Добавьте файлы проекта.

---
+++ LinesCount/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinesCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] initialDirs =
+                {
+                //@"C:\Users\User\Desktop\CAMERA 25 07"
+                @"C:\Users\User\Source\Repos\Interpolator\InterpSolution\RobotSim",
+                  @"C:\Users\User\Source\Repos\Interpolator\InterpSolution\RobotSimTests",
+                  @"C:\Users\User\Source\Repos\Interpolator\InterpSolution\RobotIM",
+                  @"C:\Users\User\Source\Repos\Interpolator\InterpSolution\RobotIMTests",
+                  @"C:\Users\User\Source\Repos\Interpolator\InterpSolution\AeroApp",
+            //@"C:\Users\User\Source\Repos\Interpolator\InterpSolution\DoubleEnumGenetic",
+            //@"C:\Users\User\Source\Repos\Interpolator\InterpSolution\Experiment",
+            //@"C:\Users\User\Source\Repos\Interpolator\InterpSolution\GeneticNik",
+            //@"C:\Users\User\Source\Repos\Interpolator\InterpSolution\GraphsFromExcel",
+            @"C:\Users\User\Source\Repos\Interpolator\InterpSolution\InterpApp",
+            //@"C:\Users\User\Source\Repos\Interpolator\InterpSolution\MassDrummer",
+            //@"C:\Users\User\Source\Repos\Interpolator\InterpSolution\MultiGenetic",
+            //@"C:\Users\User\Source\Repos\Interpolator\InterpSolution\OneDemSPH",
+            @"C:\Users\User\Source\Repos\Interpolator\InterpSolution\ReactiveODE",
+            //@"C:\Users\User\Source\Repos\Interpolator\InterpSolution\RobotSim",
+            @"C:\Users\User\Source\Repos\Interpolator\InterpSolution\SimpleIntegrator"
+            };
+
+            var t = (0,0);
+            foreach (var dir in initialDirs)
+            {
+                var (l,c) = PrintInfo(dir);
+                t.Item1 += l;
+                t.Item2 += c;
+            }
+
+            Console.WriteLine($"{"SUMM ="}  {t.Item1,5} строк  {t.Item2,5} символвов");
+            Console.ReadLine();
+
+        }
+
+        static (int,int) PrintInfo(string initialDir)
+        {
+            var allFiles = FileSearcher.Search(initialDir,".cs");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(initialDir);
+            foreach (var fp in allFiles)
+            {
+                Console.WriteLine($"{Path.GetFileName(fp),-30}  { File.ReadAllLines(fp).Length,5} строк  {File.ReadAllText(fp).Length,5} символов");
+            }
+
+            var linesSum = allFiles
+                .Select(fp => File.ReadAllLines(fp).Length)
+                .Sum();
+            var charSum = allFiles
+                .Select(fp => File.ReadAllText(fp).Length)
+                .Sum();
+            Console.WriteLine($"{"SUMM =",30}  {linesSum,5} строк  {charSum,5} символвов");
+            return (linesSum, charSum);
+        }
+
+
+    }
+}
---
+++ LinesCount/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("LinesCount")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LinesCount")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("9ecfa779-ae7a-4f2f-afe3-0dc2666a0256")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
